aeecc13bfb462efc1085c41d4f83e1c0bc684df7
Abhinava Sai Kagana <kaganaabhiteja@gmail.com>
2026-04-08 22:43:08 +0530

    Fix: add FastAPI server for HF

---
+++ app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from subprocess import run
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"status": "running"}
+
+@app.post("/reset")
+def run_env():
+    result = run(["python", "inference.py"], capture_output=True, text=True)
+    return {"output": result.stdout}
